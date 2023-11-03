7453fef7962bf0e93e4ef5bc6bb5deee43e8f5b4
Bruno S Velho <bruno.velho@gmail.com>
2023-11-02 22:21:39 -0300

    Utilizando o User Repository

--- backend-n11-rocketnotes/jest.config.js
+++ backend-n11-rocketnotes/jest.config.js
@@ -2,5 +2,5 @@ module.exports = {
   bail: true,
   coverageProvider: 'v8',
 
-  testMatch: ['<rootDir>/src/**/*.spec.js'],
+  testMatch: ['<rootDir>/**/*.spec.js'],
 }
--- backend-n11-rocketnotes/src/controllers/UsersController.js
+++ backend-n11-rocketnotes/src/controllers/UsersController.js
@@ -1,22 +1,17 @@
 const { hash, compare } = require("bcryptjs")
 const AppError = require("../utils/AppError");
 
+const UserRepository = require("../repositories/UserRepository");
 const sqliteConnection = require("../database/sqlite");
 
 class UsersController {
-  /**
-   * index - GET para listar vários registros
-   * show - GET para exibir um registro especifico.
-   * create - POST para criar um novo registro.
-   * update - PUT para atualizar um registro.
-   * delete - DELETE para excluir um registro.
-   */
 
   async create(request, response) {
     const { name, email, password } = request.body;
 
-    const database = await sqliteConnection();
-    const checkUserExists = await database.get("SELECT * FROM users WHERE email = ?", [email]);
+    const userRepository = new UserRepository()
+
+    const checkUserExists = await userRepository.findByEmail(email);
 
     if (checkUserExists) {
       throw new AppError("Este e-mail já está sendo utilizado.");
@@ -24,42 +19,40 @@ class UsersController {
 
     const hashedPassword = await hash(password, 8);
 
-    await database.run(
-      "INSERT INTO users (name, email, password) VALUES (?, ?, ?)",
-      [name, email, hashedPassword]);
+    await userRepository.create({ name, email, password: hashedPassword });
 
     response.status(201).json();
   }
 
   async update(request, response) {
-    const { name, email, password, old_password} = request.body;
+    const { name, email, password, old_password } = request.body;
     const user_id = request.user.id;
 
     const database = await sqliteConnection();
 
     const user = await database.get("SELECT * FROM users WHERE id = ?", [user_id]);
 
-    if(!user) {
+    if (!user) {
       throw new AppError("Usuário não encontrado");
     }
 
     const userWithUpdatedEmail = await database.get("SELECT * FROM users WHERE email = ?", [email]);
 
-    if(userWithUpdatedEmail && userWithUpdatedEmail.id !== user.id) {
+    if (userWithUpdatedEmail && userWithUpdatedEmail.id !== user.id) {
       throw new AppError("Este e-mail já está sendo utilizado.");
     }
 
     user.name = name ?? user.name;
     user.email = email ?? user.email;
 
-    if(password && !old_password) {
+    if (password && !old_password) {
       throw new AppError("Você precisa informar a senha antiga para atualizar a senha.");
     }
 
-    if(password && old_password) {
+    if (password && old_password) {
       const checkOldPassword = await compare(old_password, user.password);
 
-      if(!checkOldPassword) {
+      if (!checkOldPassword) {
         throw new AppError("Senha antiga incorreta.");
       }
 
