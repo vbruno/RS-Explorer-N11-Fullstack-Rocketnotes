87709906c8e239c57db857ba47ec1b8eeae6131f
Bruno S Velho <bruno.velho@gmail.com>
2023-11-04 15:41:07 -0300

    Testando e-mail em uso

--- backend-n11-rocketnotes/src/services/UserCreateService.spec.js
+++ backend-n11-rocketnotes/src/services/UserCreateService.spec.js
@@ -1,20 +1,51 @@
 const UserCreateService = require("./UserCreateService");
 const UserRepositoryInMemory = require('../repositories/UserRepositoryInMemory')
 
-it("user should be create", async () => {
-  const user = {
-    name: "John Doe",
-    email: "user@teste.com.",
-    password: '123456'
+const AppError = require("../utils/AppError");
+
+
+describe('UserCreateService', () => {
+  it("user should be create", async () => {
+    const user = {
+      name: "John Doe",
+      email: "user@teste.com.",
+      password: '123456'
+    }
+
+    const userRepositoryInMemory = new UserRepositoryInMemory();
+
+    const userCreateService = new UserCreateService(userRepositoryInMemory);
+
+    const userCreated = await userCreateService.execute(user);
+
+    console.log(userCreated)
+
+    expect(userCreated).toHaveProperty("id");
+  });
+
+  it("user not should br create with exists email", async () => {
+    const user1 = {
+      name: "User Test 1",
+      email: "user@email.com",
+      password: '123456'
+    }
+
+    const user2 = {
+      name: "User Test 2",
+      email: "user@email.com",
+      password: '123456'
+    }
+
+    const userRepositoryInMemory = new UserRepositoryInMemory();
+
+    const userCreateService = new UserCreateService(userRepositoryInMemory);
+
+    await userCreateService.execute(user1);
+
+    expect(async () => {
+      await userCreateService.execute(user2)
+    }).rejects.toEqual(new AppError("Este e-mail já está sendo utilizado."))
   }
+  );
+})
 
-  const userRepositoryInMemory = new UserRepositoryInMemory();
-
-  const userCreateService = new UserCreateService(userRepositoryInMemory);
-
-  const userCreated = await userCreateService.execute(user);
-
-  console.log(userCreated)
-
-  expect(userCreated).toHaveProperty("id");
-});
