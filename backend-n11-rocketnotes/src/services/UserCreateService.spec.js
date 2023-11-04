86ba5027090e771bac549587e8eb457630a17438
Bruno S Velho <bruno.velho@gmail.com>
2023-11-04 15:27:55 -0300

    Users Repository In Memory

--- backend-n11-rocketnotes/src/services/UserCreateService.spec.js
+++ backend-n11-rocketnotes/src/services/UserCreateService.spec.js
@@ -1,17 +1,20 @@
 const UserCreateService = require("./UserCreateService");
+const UserRepositoryInMemory = require('../repositories/UserRepositoryInMemory')
 
-it("user should be create", () => {
+it("user should be create", async () => {
   const user = {
     name: "John Doe",
     email: "user@teste.com.",
     password: '123456'
   }
 
-  
+  const userRepositoryInMemory = new UserRepositoryInMemory();
 
-  const userCreateService = new UserCreateService();
+  const userCreateService = new UserCreateService(userRepositoryInMemory);
 
-  const userCreated = userCreateService.execute(user);
+  const userCreated = await userCreateService.execute(user);
+
+  console.log(userCreated)
 
   expect(userCreated).toHaveProperty("id");
 });
